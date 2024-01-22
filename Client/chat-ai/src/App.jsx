1fa40c6d8dfdb0f73bd0e1efab936d0917c9820c
krsonuC2od <190101120062@cutm.ac.in>
2024-01-22 19:57:12 +0530

    created navbar and otherpages and also add theme part

--- Client/chat-ai/src/App.jsx
+++ Client/chat-ai/src/App.jsx
@@ -1,8 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 const App = () => {
+  const theme = useMemo(() => createTheme(themeSettings(), []));
   return (
     <>
-      <h1>Hello Sonu </h1>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </ThemeProvider>
     </>
   );
-  }
-export default  App;
+};
+export default App;
