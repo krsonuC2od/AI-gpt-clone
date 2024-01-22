c3ba6ec9bfeaf1ca97de6adb3009ca9e4f91ae6b
krsonuC2od <190101120062@cutm.ac.in>
2024-01-22 15:19:43 +0530

    implement ErrorHandling and completed register login and logout controller

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const morgan  = require('morgan');
-const cors  = require('cors');
-const bodyParser = require('body-parser');
-const colors = require('colors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/connection');
-const PORT = process.env.PORT||9090;
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/connection");
+const PORT = process.env.PORT || 9090;
+const errorHandler = require("./middelwares/errorMiddleware");
+const authRoutes = require("./routes/authRoutes");
 
- 
 // config dotenv
 dotenv.config();
 
@@ -18,13 +19,19 @@ connectDB();
 //app object
 const app = express();
 
-//middleware 
+//middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(errorHandler);
 
+//Api Routes
+app.use("/api/v1/auth", authRoutes);
 //listen app
-app.listen(PORT,()=>{
-    console.log(`server is running in ${process.env.Dev_mode} mode on port no : ${PORT}`.bgCyan.white);
-})
+app.listen(PORT, () => {
+  console.log(
+    `server is running in ${process.env.Dev_mode} mode on port no : ${PORT}`
+      .bgCyan.white
+  );
+});
